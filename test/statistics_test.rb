aee7bb9a7743c34e9497fd761a5c0f96e3deb575
Joshua Toyota <jearsh@gmail.com>
2014-04-18 00:50:13 -0400

    auto-committing to show my workflow

--- test/statistics_test.rb
+++ test/statistics_test.rb
@@ -7,5 +7,10 @@ describe Statistics do
   it 'should have a Statistics class' do
     Statistics.is_a? Class
   end
+
+  describe '#batting_average' do
+    it 'calculates the proper batting average'
+    it 'returns a float'
+  end
 end
 

85ea4fe6ef93e251383ce8ca486d269ec69d50b6
Joshua Toyota <jearsh@gmail.com>
2014-04-18 01:37:50 -0400

    auto-committing to show my workflow

--- test/statistics_test.rb
+++ test/statistics_test.rb
@@ -54,60 +54,58 @@ abreubo01,2010,AL,LAA,154,573,88,146,41,1,20,78,24,10
 abreubo01,2009,AL,LAA,152,563,96,165,29,3,15,103,30,8
 CSV_DATA
 
-describe BattingFile do
+describe 'Exercise' do
   let(:batting_file) { BattingFile.load(SAMPLE_DATA) }
-  let(:player_id) { 'abreubo01' }
-  it 'should have a BattingFile class' do
-    BattingFile.must_be_instance_of Class
-  end
-
-  describe 'BattingFile.load(csv_data)' do
-    it 'should return a new BattingFile instance' do
-      batting_file.must_be_instance_of BattingFile
-    end
-
-    it 'should contain an array with 2 elements' do
-      batting_file.rows.count.must_equal 2, "#{batting_file.rows.count} is not 2"
-    end
-
-    it 'should return 1 player class' do
-      batting_file.players.count.must_equal 1
-    end
-
-    it 'properly loads a player with > 1 year of stats' do
-      player = batting_file.players[player_id]
-      player.years.count.must_equal 2
-    end
-  end
-end
-
-describe Player do
   let(:player_id) { 'abreubo01' }
   let(:player) { Player.new(player_id) }
-  let(:batting_file) { BattingFile.load(SAMPLE_DATA) }
   let(:year) { batting_file.rows.first }
 
-  describe '#add_year' do
-    it 'should have 0 starting years' do
-      player.years.count.must_equal 0
+  describe BattingFile do
+    it 'should have a BattingFile class' do
+      BattingFile.must_be_instance_of Class
     end
 
-    it 'year could should increase by 1' do
-      player.add_year year
-      player.years.count.must_equal 1
+    describe 'BattingFile.load(csv_data)' do
+      it 'should return a new BattingFile instance' do
+        batting_file.must_be_instance_of BattingFile
+      end
+
+      it 'should contain an array with 2 elements' do
+        batting_file.rows.count.must_equal 2, "#{batting_file.rows.count} is not 2"
+      end
+
+      it 'should return 1 player class' do
+        batting_file.players.count.must_equal 1
+      end
+
+      it 'properly loads a player with > 1 year of stats' do
+        a_player = batting_file.players[player_id]
+        a_player.years.count.must_equal 2
+      end
     end
   end
-end
 
-describe Year do
-  let(:batting_file) { BattingFile.load(SAMPLE_DATA) }
-  let(:player_id) { 'abreubo01' }
-  it 'should properly load all stats' do
+  describe Player do
+    describe '#add_year' do
+      it 'should have 0 starting years' do
+        player.years.count.must_equal 0
+      end
 
+      it 'year could should increase by 1' do
+        player.add_year year
+        player.years.count.must_equal 1
+      end
+    end
   end
-  describe '#batting_average' do
-    it 'calculates the proper batting average'
-    it 'returns a float'
+
+  describe Year do
+    it 'should properly load all stats' do
+
+    end
+    describe '#batting_average' do
+      it 'calculates the proper batting average'
+      it 'returns a float'
+    end
   end
 end
 
